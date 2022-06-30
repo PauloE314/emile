30f3953d20fe5923d81c438f386e2b8bb1d6b215
Paulo <pauloeduardodelima155@gmail.com>
2022-06-30 11:18:17 -0300

    Improves user tests to add #favorited?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,4 +45,35 @@ RSpec.describe User, type: :model do
   context 'when data is correct' do
     it { is_expected.to be_valid }
   end
+
+  describe '#favorited?' do
+    let(:recipe_data) do
+      {
+        name: 'Recipe',
+        creator: subject,
+        time: 10,
+        servings: 5,
+        category: 'brazilian',
+        image: nil,
+        steps_attributes: [],
+        ingredients_attributes: [],
+      }
+    end
+    let(:favorited_recipe) { Recipe.create(recipe_data) }
+    let(:not_favorited_recipe) { Recipe.create(recipe_data) }
+
+    before { Favorite.new(user: subject, recipe: favorited_recipe).save }
+    
+    context 'when user has favorited the recipe' do
+      it 'returns true' do
+        expect(subject.favorited? favorited_recipe).to be_truthy
+      end      
+    end
+
+    context 'when user has not favorited the recipe' do
+      it 'returns true' do
+        expect(subject.favorited? not_favorited_recipe).to be_falsy
+      end      
+    end
+  end
 end
